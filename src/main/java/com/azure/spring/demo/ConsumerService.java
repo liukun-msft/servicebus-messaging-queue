4620ca3a8f8427b8a3bb2f6ec715cbe656f4dd80
Kun Liu <liuku@microsoft.com>
2022-09-26 16:44:06 +0800

    Update ConsumerService to test multiple queues

--- src/main/java/com/azure/spring/demo/ConsumerService.java
+++ src/main/java/com/azure/spring/demo/ConsumerService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 @Service
 public class ConsumerService {
@@ -16,9 +18,112 @@ public class ConsumerService {
 	 * The concurrency parameter sets the concurrency property of {@link ServiceBusProcessorClient}.
 	 * @param message
 	 */
-	@ServiceBusListener(destination = "SERVICE_BUS_QUEUE_NAME", concurrency = "50")
-	public void handleMessageFromServiceBus(String message) {
+	@ServiceBusListener(destination = "stress-test-queue1", concurrency = "10")
+	public void handleMessageFromServiceBus1(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
 		log.info("Consume message: '{}'", message);
 	}
+	@ServiceBusListener(destination = "stress-test-queue2", concurrency = "10")
+	public void handleMessageFromServiceBus2(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue3", concurrency = "10")
+	public void handleMessageFromServiceBus3(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue4", concurrency = "10")
+	public void handleMessageFromServiceBus4(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue5", concurrency = "10")
+	public void handleMessageFromServiceBus5(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue6", concurrency = "10")
+	public void handleMessageFromServiceBus6(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue7", concurrency = "10")
+	public void handleMessageFromServiceBus7(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue8", concurrency = "10")
+	public void handleMessageFromServiceBus8(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue9", concurrency = "10")
+	public void handleMessageFromServiceBus9(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue10", concurrency = "10")
+	public void handleMessageFromServiceBus10(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue11", concurrency = "10")
+	public void handleMessageFromServiceBus11(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue12", concurrency = "10")
+	public void handleMessageFromServiceBus12(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue13", concurrency = "10")
+	public void handleMessageFromServiceBus13(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue14", concurrency = "10")
+	public void handleMessageFromServiceBus14(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue15", concurrency = "10")
+	public void handleMessageFromServiceBus15(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue16", concurrency = "10")
+	public void handleMessageFromServiceBus16(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue17", concurrency = "10")
+	public void handleMessageFromServiceBus17(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue18", concurrency = "10")
+	public void handleMessageFromServiceBus18(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue19", concurrency = "10")
+	public void handleMessageFromServiceBus19(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+	@ServiceBusListener(destination = "stress-test-queue20", concurrency = "10")
+	public void handleMessageFromServiceBus20(String message) throws InterruptedException {
+		TimeUnit.SECONDS.sleep(1);
+		log.info("Consume message: '{}'", message);
+	}
+
+
+
+
+
+
+
 
 }
